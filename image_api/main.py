cd6bb11fc9a2014c7e8a8cf325d20ebb870e436c
Not A Number <nan140114@gmail.com>
2022-02-07 16:32:52 -0500

    adding cors

--- image_api/main.py
+++ image_api/main.py
@@ -2,9 +2,20 @@ from fastapi import FastAPI, File, Form, UploadFile
 from utils.files import create_temp_file, get_image_attr
 from utils.storage import save_image_to_private
 from models.image import Image
+from fastapi.middleware.cors import CORSMiddleware
 import json
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/images")
 def get_all_images():
